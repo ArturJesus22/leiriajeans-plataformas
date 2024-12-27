b08be8012a01d1636e1b596f008f360e91cea9ac
Artur Jesus <2220866@my.ipleiria.pt>
2024-12-27 10:56:24 +0000

    API QUASE PRONTA;
    PREPARAR COMEÇAR COM UNIT TEST;

---
+++ leiriajeans/backend/modules/api/controllers/AuthController.php
@@ -0,0 +1,96 @@
+<?php
+namespace backend\modules\api\controllers;
+use yii\base\Behavior;
+use yii\filters\auth\QueryParamAuth;
+use yii\rest\ActiveController;
+use yii;
+use yii\web\Controller;
+use backend\modules\api\components\CustomAuth;
+
+class AuthController extends ActiveController
+{
+    public $modelClass = 'common\models\User'; //model default user
+    public $modelUserForm = 'common\models\UsersForm'; //model dos dados do utilizador
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        return $behaviors;
+    }
+
+
+    public function actionLogin() {
+        // Obter os dados
+        $username = Yii::$app->request->post('username');
+        $password = Yii::$app->request->post('password');
+
+        // Validar os dados de entrada
+        if (empty($username) || empty($password)) {
+            return ['message' => 'Todos os campos são obrigatórios.'];
+        }
+
+        $user = $this->modelClass::find()->where(['username' => $username])->one();
+        if ($user == null) {
+            throw new \yii\web\NotFoundHttpException("Não existe o utilizador " . $username);
+        }
+
+        if ($user->validatePassword($password)) {
+            return ['message' => 'Autenticado com sucesso'];
+        } else {
+            return ['message' => 'Erro na autenticação'];
+        }
+    }
+
+    public function actionSignup() {
+
+        // Obter os dados da requisição
+        $username = Yii::$app->request->post('username');
+        $email = Yii::$app->request->post('email');
+        $password = Yii::$app->request->post('password_hash');
+        $nome = Yii::$app->request->post('nome');
+        $codpostal = Yii::$app->request->post('codpostal');
+        $localidade = Yii::$app->request->post('localidade');
+        $rua = Yii::$app->request->post('rua');
+        $nif = Yii::$app->request->post('nif');
+        $telefone = Yii::$app->request->post('telefone');
+        $role = ('cliente');
+
+        // Validar dados de entrada
+        if (empty($nome) || empty($username) || empty($password) ||empty($email)  || empty($codpostal) || empty($localidade) || empty($rua) || empty($nif) || empty($telefone)) {
+            return ['message' => 'Todos os campos são obrigatórios.'];
+        }
+
+        // Criar o novo utilizador
+        $userModel = new $this->modelClass;
+        $userModel->username = $username;
+        $userModel->email = $email;
+        $userModel->role = $role;
+        $userModel->setPassword($password);
+        $userModel->generateAuthKey();
+        $userModel->generateEmailVerificationToken();
+
+
+
+        // guardar o utilizador
+        if ($userModel->save()) {
+            // Após guardar o utilizador, criar o perfil associado
+            $userFormModel = new $this->modelUserForm;
+            $userFormModel->user_id = $userModel->id; // associar o perfil ao utilizador
+            $userFormModel->nome = $nome;
+            $userFormModel->codpostal = $codpostal;
+            $userFormModel->localidade = $localidade;
+            $userFormModel->rua = $rua;
+            $userFormModel->nif = $nif;
+            $userFormModel->telefone = $telefone;
+
+            // guardar o perfil do utilizador
+            if ($userFormModel->save()) {
+                return ['message' => 'Utilizador e perfil criados com sucesso'];
+            } else {
+                return ['message' => 'Erro ao criar o perfil do Utilizador'];
+            }
+        } else {
+            return ['message' => 'Erro ao criar o utilizador'];
+        }
+    }
+}
